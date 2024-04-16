a8f66f48450275c42cbe889355966d536623343c
Matheus Fortini <matheusfortini7@gmail.com>
2024-04-15 23:11:03 -0300

    corrige a estrutura das classes para que reconheça o AuthenticatinError

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -1,22 +1,31 @@
 module Api
   module V1
     class AuthenticationController < ApplicationController
-      rescue_from ActionController::ParameterMissing, with: :parameter_missing
+      class AuthenticationError < StandardError; end
 
-      def create
-        p params.require(:password).inspect
+        rescue_from ActionController::ParameterMissing, with: :parameter_missing
+        rescue_from AuthenticationError, with: :handle_unauthenticated
 
-        user = User.find_by(username: params.require(:username))
-        token =  AuthenticationTokenService.call(user.id)
+        def create
+          raise AuthenticationError unless user.authenticate(params.require(:password))
+          token = AuthenticationTokenService.call(user.id)
 
-        render json: { token: token }, status: :created
+          render json: { token: token }, status: :created
+        end
+
+      private
+
+      def user
+        @user ||= User.find_by(username: params.require(:username))
       end
-    end
 
-    private
+      def parameter_missing(e)
+        render json: { error: e.message }, status: :unprocessable_entity
+      end
 
-    def parameter_missing(e)
-      render json: { error: e.message }, status: :unprocessable_entity
+      def handle_unauthenticated
+        head :unauthorized
+      end
     end
   end
 end
